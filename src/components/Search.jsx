e1a27a10395d6a926441db0924af1c562957604f
Denis Vakulenko <woof88@yandex.ru>
2022-08-30 17:34:01 +0500

    feat: added Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,3 +1,43 @@
-function Search({ callback }) {}
+import { useState } from "react";
+
+function Search({ callback }) {
+  const [value, setValue] = useState("");
+
+  const handleKey = (evt) => {
+    if (evt.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  const handleSubmit = () => {
+    callback(value);
+  };
+
+  return (
+    <div className="row">
+      <div className="input-field col s12">
+        <input
+          type="search"
+          id="search-field"
+          placeholder="search"
+          onKeyDown={handleKey}
+          onChange={(evt) => setValue(evt.target.value)}
+          value={value}
+        />
+        <button
+          className="btn"
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+          }}
+          onClick={handleSubmit}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export { Search };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,53 @@
 import { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { getAllCategories } from "../api";
 import { CategoryList } from "../components/CategoryList";
 import { Preloader } from "../components/Preloader";
+import { Search } from "../components/Search";
 
 function Home() {
   const [catalog, setCatalog] = useState([]);
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const { pathname, search } = useLocation();
+  const { push } = useHistory();
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.strCategory.toLowerCase().includes(str.toLowerCase())
+      )
+    );
+    push({
+      pathname,
+      search: `?search=${str}`,
+    });
+  };
 
   useEffect(() => {
     getAllCategories().then((data) => {
       setCatalog(data.categories);
+      setFilteredCatalog(
+        search
+          ? data.categories.filter((item) =>
+              item.strCategory
+                .toLowerCase()
+                .includes(search.split("=")[1].toLowerCase())
+            )
+          : data.categories
+      );
     });
   }, []);
 
   return (
-    <>{!catalog.length ? <Preloader /> : <CategoryList catalog={catalog} />}</>
+    <>
+      <Search callback={handleSearch} />
+      {!catalog.length ? (
+        <Preloader />
+      ) : (
+        <CategoryList catalog={filteredCatalog} />
+      )}
+    </>
   );
 }
 
